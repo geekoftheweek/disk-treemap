66b02368f9a7876724e8d94e665de498b76df4f1
geekoftheweek <francis.141@osu.edu>
2013-10-09 20:00:22 -0400

    Remove dependency on du.

--- treemap.py
+++ treemap.py
@@ -4,40 +4,59 @@ Creates an html treemap of disk usage, using the Google Charts API
 '''
 
 import json
-import os.path
+import os
 import subprocess
 import sys
 
-def get_usage(path):
-    '''Returns the output of running the 'du' utility on path'''
-    result = subprocess.Popen(['du', path], stdout=subprocess.PIPE)
-    return result.communicate()[0]
-
-def du2json(du_output):
-    '''Converts the output of 'du' to JSON suitable for the Google Charts
-    API'''
-    result = [['Path', 'Parent', 'Usage']]
-    seen_parents = set()
-    for line in du_output.split('\n'):
+def memoize(fn):
+    stored_results = {}
+    def memoized(*args):
         try:
-            size, path = line.strip().split('\t')
-            node_id = path
-            parent_id = os.path.dirname(path)
-            seen_parents.add(node_id)
-            node_size = int(size)
-            row = [node_id, parent_id, node_size]
-            result.append(row)
-        except ValueError:
-            continue
+            return stored_results[args]
+        except KeyError:
+            result = stored_results[args] = fn(*args)
+            return result
 
-    for i in range(len(result)):
-        path, parent, usage = result[i]
-        if parent != 'Parent' and parent not in seen_parents:
-            parent = None
-            result[i] = [path, parent, usage]
+    return memoized
 
 
+@memoize
+def get_folder_size(folder):
+    total_size = os.path.getsize(folder)
+    for item in os.listdir(folder):
+        itempath = os.path.join(folder, item)
+        if os.path.isfile(itempath):
+            total_size += os.path.getsize(itempath)
+        elif os.path.isdir(itempath):
+            total_size += get_folder_size(itempath)
+    return total_size
+
+
+def usage_iter(root):
+    root = os.path.abspath(root)
+    root_size = get_folder_size(root)
+    root_string = "{0}\n{1}".format(root, root_size)
+    yield [root_string, None, root_size]
+    
+    for parent, dirs, files in os.walk(root):
+        for dirname in dirs:
+            fullpath = os.path.join(parent, dirname)
+            try:
+                this_size = get_folder_size(fullpath)
+                parent_size = get_folder_size(parent)
+                this_string = "{0}\n{1}".format(fullpath, this_size)
+                parent_string = "{0}\n{1}".format(parent, parent_size)
+                yield [this_string, parent_string, this_size]
+            except OSError:
+                continue
+
+
+def json_usage(root):
+    root = os.path.abspath(root)
+    result = [['Path', 'Parent', 'Usage']]
+    result.extend(entry for entry in usage_iter(root))
     return json.dumps(result)
+
 
 def main(args):
     '''Populates an html template using JSON-formatted output from the
@@ -62,9 +81,11 @@ def main(args):
 
   <body>
     <div id="chart_div" style="width: 900px; height: 500px;"></div>
+    <p style="text-align: center">Click to descend. Right-click to ascend.</p>
   </body>
 </html>
-    ''' % du2json(get_usage(args[0]))
+    ''' % json_usage(args[0])
+#    ''' % du2json(get_usage(args[0]))
     print html
 
 if __name__ == "__main__":
